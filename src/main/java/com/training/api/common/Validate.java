9bfb87b5aa815e3d54e0318c88d288908e16d59c
huynhctd <caotrandinhhuynh@luvina.net>
2020-01-15 16:37:55 +0700

    commit fix message exp of validate format

--- src/main/java/com/training/api/common/Validate.java
+++ src/main/java/com/training/api/common/Validate.java
@@ -21,7 +21,7 @@ public class Validate {
          */
         public static void checkFormat (String code) {
             if((code.matches("(?=^(\\D*\\d\\D*){7}$)(^(\\d{1,3}(-\\d{1,3}){2,6})$)")) == false) {
-                throw new IllegalArgumentException("Code must be include numbers of no more than 3 consecutive numbers separated by \\\"-\\\" Ex: xxx-xx-x-x\"");
+                throw new IllegalArgumentException("Code must be include numbers of no more than 3 consecutive numbers separated by \"-\" Ex: xxx-xx-x-x\"");
             }
         }
 
